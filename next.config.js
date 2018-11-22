60940da149b20d51fdcf13607bf3138ab4dbd9ab
maureenv <maureenvogel@outlook.com>
2018-11-22 00:48:39 -0500

    Add out folder

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/compare-stocks' : '',
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }
+    }
+  }
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Stock Comparer
+
+## access static app
+```
+cd out
+serve -p 8080
+```
