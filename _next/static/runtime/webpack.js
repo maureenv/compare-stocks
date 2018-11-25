3fa99bf8b52955df7005228f0e66f818c4a2962b
maureenv <maureenvogel@outlook.com>
2018-11-24 22:21:38 -0500

    Save exported files

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0e1e45950f6547b774c7";
+/******/ 	var hotCurrentHash = "fcba302832096003180d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
